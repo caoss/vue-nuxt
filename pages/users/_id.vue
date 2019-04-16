28b0f941fc04a84c0191b9d58bf75a1dc8aa3586
css <neetsCC7899>
2019-04-16 17:07:50 +0800

    css

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,14 +1,21 @@
 <template>
   <section class="container">
-    <div>user--id</div>
+    <div>user--id {{ id }}</div>
   </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      id:this.$route.params.id
+    }
+  },
   validate({ params }) {
     // 必须是number类型
     return /^\d+$/.test(params.id);
-  }
+  },
+  created() {
+  },
 };
 </script>
